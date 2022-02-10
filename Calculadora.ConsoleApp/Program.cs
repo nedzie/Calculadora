537cddf785f5dfdffeb5bd31bbba1e29f3565258
nedzie <marcos-elivelton@hotmail.com>
2022-02-10 12:43:07 -0300

    Atualização: ReadKey() ao invés de ReadLine();

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -3,8 +3,8 @@
  * requisito 3 ok
  * requisito 4 ok
  * requisito 5 ok
- * requisito 6 em andamento
- * 
+ * requisito 6 ok
+ * requi 7 ok
  */
 
 using System;
@@ -16,8 +16,10 @@ namespace Calculadora.ConsoleApp
         static void Main(string[] args)
         {
             // Variáveis
-            string option;
-            int regOps;
+
+            string option, opSelecionado = "";
+            string[] historico = new string[100];
+            int contador = 0;
 
             // Entradas
 
@@ -36,7 +38,7 @@ namespace Calculadora.ConsoleApp
 
 
                 // Verificará se foi selecionado algo inválido
-                if(option != "1"  && option != "2" && option != "3" && option != "4" && option != "5" && option != "s")
+                if (option != "1" && option != "2" && option != "3" && option != "4" && option != "5" && option != "s")
                 {
                     Console.Clear();
                     Console.Beep();
@@ -46,10 +48,35 @@ namespace Calculadora.ConsoleApp
                     continue; // Continue1
                 }
 
-                if(option == "5")
+                if (option == "5")
                 {
+                    if (contador == 0)
+                    {
+                        Console.WriteLine("Ainda não foram feitas operações");
+                    }
+                    else
+                    {
+                        foreach (var x in historico)
+                        {
+                            if (x == null)
+                            {
+                                continue;
+                            }
+                            else
+                            if (x.Contains("+"))
+                                Console.ForegroundColor = ConsoleColor.Green;
+                            else if (x.Contains("-"))
+                                Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(x);
+                            Console.ResetColor();
+                        }
+                    }
 
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
                 }
+
 
                 // Para sair do programa
                 if (option == "s")
@@ -60,23 +87,27 @@ namespace Calculadora.ConsoleApp
                 Console.Write("Digite o 1º número: ");
                 string strPrimeiroNumero = Console.ReadLine(); // Faz a leitura
 
+            voltaUm:
                 Console.Write("Digite o 2º número: ");
                 string strSegundoNumero = Console.ReadLine(); // Faz a leitura
 
-                int primeiroNumero = Convert.ToInt32(strPrimeiroNumero);   // Fazem as
-                int segundoNumero = Convert.ToInt32(strSegundoNumero);     // conversões necessárias
-                int res = 0;
+                double primeiroNumero = double.Parse(strPrimeiroNumero);   // Fazem as
+                double segundoNumero = double.Parse(strSegundoNumero);  // conversões necessárias
+                double res = 0;
 
                 if (option == "1")
                 {
+                    opSelecionado = "+";
                     res = primeiroNumero + segundoNumero; // Soma os dois valores
                 }
                 else if (option == "2")
                 {
+                    opSelecionado = "-";
                     res = primeiroNumero - segundoNumero; // Subtrai os dois valores
                 }
                 else if (option == "3")
                 {
+                    opSelecionado = "*";
                     res = primeiroNumero * segundoNumero; // Multiplica os dois valores
                 }
                 else if (option == "4")
@@ -84,17 +115,20 @@ namespace Calculadora.ConsoleApp
                     if (segundoNumero == 0)
                     {
                         Console.WriteLine("Erro!");
-                        break;
+                        goto voltaUm;
                     }
+                    opSelecionado = "/";
                     res = primeiroNumero / segundoNumero; // Divide os dois valores
                 }
 
-                Console.Write("O resultado é: " + res + "\n");
+                historico[contador] = primeiroNumero + " " + opSelecionado + " " + segundoNumero + " = " + res;
+                contador++;
+
+                Console.Write($"O resultado é: {res:F2}\n");
                 Console.WriteLine("Pressione 'Enter' para continuar... ");
-                Console.ReadLine();
+                Console.ReadKey();
 
             }
-
         } // Fecha main
     } // Fecha internal
 } // Fecha geral
